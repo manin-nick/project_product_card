dfe30d67570921132c2acf74217ef7e8d67e9e66
manin-nick <manin.nick@gmail.com>
2024-09-02 21:08:38 +0400

    first commit

---
+++ Desktop/буткемп/project_jc/index.js
@@ -0,0 +1,54 @@
+// Переменные
+
+// Задача: Простое объявление переменной
+let greeting = "Hello, World!";
+console.log (greeting);
+
+// Задача: Работа с числами
+let a = 5;
+let b = 10;
+let sum = a + b;
+console.log (sum);
+
+// Задача: Изменение значения переменной
+let name = "Alice";
+name ="Bob";
+console.log (name);
+
+// Типы данных
+
+// Задача: Определение типа данных
+let string = "Hello";
+let number = 42;
+let boolean = true;
+console.log (typeof string);
+console.log (typeof number);
+console.log (typeof boolean);
+
+// Задача: Преобразование данных в строку
+let age = 25;
+let str = String(age);
+console.log (str);
+
+// Задача: Объединение строк
+let firstName = "John";
+let lastName = "Doe";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+// Преобразование типов
+
+// Задача: Преобразование строки в число
+let strNumber = "100";
+let num = Number(strNumber);
+console.log(num);
+
+// Задача: Преобразование числа в строку
+let score = 95;
+let STR = String(score);
+console.log(STR);
+
+// Задача: Преобразование булевого значения в строку
+let isLoggedIn = true;
+let logicValue = Boolean(isLoggedIn);
+console.log(logicValue);
